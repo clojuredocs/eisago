d4f8fa428e37d2a0a679dd2ac55a03769372d0b2
Connor Mendenhall <ecmendenhall@gmail.com>
2013-09-09 15:15:16 -0500

    Use Clojure 1.5.1. Remove clabango dependency.

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths ["etc"]
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "4.0.4"]
-                 [clabango "0.3"]
                  [clj-http "0.5.7"]
                  [commons-codec "1.6"]
                  [laeggen "0.4"]
                  [org.apache.lucene/lucene-queryparser "4.0.0"]
-                 [sonian/carica "1.0.0"]]
+                 [sonian/carica "1.0.0"]
+                 [ring "1.2.0"]]
   :profiles {:dev {:dependencies [[org.clojure/java.jdbc "0.2.3"]
                                   [mysql/mysql-connector-java "5.1.6"]
                                   [speclj "2.5.0"]]}}
